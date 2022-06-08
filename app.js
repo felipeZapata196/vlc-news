ab5925dde062e7379884975d79a9b6dbffd2e2e3
felipe <[an-felipe@hotmail.es]>
2022-06-08 15:24:07 +0200

    dd

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ const app = express();
 
 app.use(express.json()); 
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
-
 
 app.use(express.static(path.join(__dirname, 'public')));
 
