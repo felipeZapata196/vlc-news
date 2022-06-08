dc87c81d084b2ea60a7f658c1fef37eb4309bbf8
felipe <[an-felipe@hotmail.es]>
2022-06-08 14:29:30 +0200

    commit

--- server.js
+++ server.js
@@ -16,9 +16,12 @@ mongoose
     console.log("connection succesfull")
 })
 
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(express.static(
-    path.join(__dirname,'../client/build')));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
 
 
 
